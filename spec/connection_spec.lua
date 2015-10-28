c8280c8e6a6014d18c7a5ac31a89b7039583a0cd
Adam Grandquist <grandquista@gmail.com>
2015-10-28 07:00:39 -0700

    Create test for #7.

--- spec/connection_spec.lua
+++ spec/connection_spec.lua
@@ -7,6 +7,7 @@ describe('connection', function()
       assert.are_not.equal(c, nil)
     end)
   end)
+
   it('return conn', function()
     local conn = r.connect()
     assert.are_not.equal(conn, nil)
@@ -14,10 +15,36 @@ describe('connection', function()
     conn:close()
     assert.are.equal(conn:open(), false)
   end)
+
   it('basic_pool', function()
     r.pool({}, function(err, p)
       if err then error(err.message) end
       assert.are_not.equal(p, nil)
     end)
+  end)
+
+  it('fails to insert eventually', function()
+    local reql_db = 'connection'
+    local reql_table = 'tests'
+
+    local c, err = r.connect()
+    if err then error(err.message) end
+
+    r.db_create(reql_db):run(c)
+    c:use(reql_db)
+    r.table_create(reql_table):run(c)
+
+    assert.has_error(
+      function()
+        for _id=1,500000 do
+          r.table(reql_table):insert({id=_id}):run(c)
+        end
+      end,
+      ''
+    )
+
+    c:reconnect()
+    r.table(reql_table):delete():run(c)
+    c:close()
   end)
 end)
