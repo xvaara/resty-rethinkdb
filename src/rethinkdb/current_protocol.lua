b01b1262aec8272b45e94bc3744932bd5876fcac
Adam Grandquist <grandquista@gmail.com>
2016-04-27 18:50:17 -0700

    Add basic checks to get through the first server response.

--- src/rethinkdb/current_protocol.lua
+++ src/rethinkdb/current_protocol.lua
@@ -20,19 +20,38 @@ function m.init(_r)
 
     -- Now we have to wait for a response from the server
     -- acknowledging the connection
+    -- this will be a null terminated json document on success
+    -- or a null terminated error string on failure
     while 1 do
       local buf, err = raw_socket.recv()
       if not buf then
         return nil, err
       end
       buffer = buffer .. buf
-      local i, j = buf:find('\0')
+      local i, _ = buf:find('\0')
       if i then
         local status_str = buffer:sub(1, i - 1)
         buffer = buffer:sub(i + 1)
         print(status_str)
-        return nil, err
+        local response = pcall(_r.decode, status_str)
+        if response == nil then
+          return nil, status_str
+        end
+        break
       end
+    end
+
+    -- when protocol versions are updated this is where we send the following
+    -- for now it is sent above
+    -- {
+    --   "protocol_version": <number>,
+    --   "authentication_method": <method>,
+    --   "authentication": "n,,n=<user>,r=<nonce>"
+    -- }
+
+    -- wait for the second server challenge
+    -- this is always a json document
+    while 1 do
     end
   end
 end
