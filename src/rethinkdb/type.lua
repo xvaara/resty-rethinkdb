db233ee819a0a20cbaa8da5c9d7fd3efe4a567c6
Adam Grandquist <grandquista@gmail.com>
2016-05-09 19:48:30 -0700

    Change object type testing.

---
+++ src/rethinkdb/type.lua
@@ -0,0 +1,32 @@
+return function(obj)
+  if type(obj) ~= 'table' or getmetatable(obj) == nil then return nil end
+
+  if type(obj.build) == 'function' and type(obj.compose) == 'function' then
+    return 'reql'
+  end
+
+  if type(obj._start) == 'function' and type(obj.use) == 'function' then
+    if obj.connect == nil then
+      return 'pool'
+    end
+
+    if type(obj.noreply_wait) == 'function' then
+      return 'connection'
+    end
+
+    return 'connector'
+  end
+
+  if type(obj.each) == 'function' and type(obj.to_array) == 'function' then
+    return 'cursor'
+  end
+
+  if type(obj.msg) == 'string' and type(obj.message) == 'function' then
+    local success, message = pcall(obj.message)
+    if success and type(message) == 'string' then
+      return 'error'
+    end
+  end
+
+  return nil
+end
