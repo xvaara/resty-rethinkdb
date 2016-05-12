062dc1a38c008808cee66504e8070e755f83dcaa
Adam Grandquist <grandquista@gmail.com>
2016-05-12 05:27:56 -0700

    Add tests to ensure server exists.

---
+++ spec/sane_spec.lua
@@ -0,0 +1,10 @@
+insulate('safety net', function()
+  describe('environment #sanity', function()
+    it('is sane', function()
+      local socket = require('socket')
+      local client = assert(socket.tcp())
+
+      assert(client:connect('localhost', 28015))
+    end)
+  end)
+end)
