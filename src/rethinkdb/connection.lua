7ffb1e5e8e38bf699a485dc446480be91fd7b466
Adam Grandquist <grandquista@gmail.com>
2016-05-14 11:18:18 -0700

    Whitespace.

--- src/rethinkdb/connection.lua
+++ src/rethinkdb/connection.lua
@@ -7,30 +7,30 @@ local DEFAULT_USER = 'admin'
 local DEFAULT_AUTH_KEY = ''
 local DEFAULT_TIMEOUT = 20 -- In seconds
 
-  return function(opts, _proto_version)
-    local port = opts.port or DEFAULT_PORT
-    local db = opts.db -- left nil if this is not set
-    local auth_key = opts.password or opts.auth_key or DEFAULT_AUTH_KEY
-    local user = opts.user or DEFAULT_USER
-    local timeout = opts.timeout or DEFAULT_TIMEOUT
-    local ssl_params = opts.ssl
-    local proto_version = _proto_version or current_protocol
-    local host = opts.host or DEFAULT_HOST
-    local r = opts.r or {}
+return function(opts, _proto_version)
+  local port = opts.port or DEFAULT_PORT
+  local db = opts.db -- left nil if this is not set
+  local auth_key = opts.password or opts.auth_key or DEFAULT_AUTH_KEY
+  local user = opts.user or DEFAULT_USER
+  local timeout = opts.timeout or DEFAULT_TIMEOUT
+  local ssl_params = opts.ssl
+  local proto_version = _proto_version or current_protocol
+  local host = opts.host or DEFAULT_HOST
+  local r = opts.r or {}
 
-    local function connect(callback)
-      return connection_instance(
-        r, auth_key, db, host, port, proto_version, ssl_params, timeout, user
-        ).connect(callback)
-    end
-
-    return {
-      _start = function(...)
-        return connect()._start(...)
-      end,
-      connect = connect,
-      use = function(_db)
-        db = _db
-      end
-    }
+  local function connect(callback)
+    return connection_instance(
+      r, auth_key, db, host, port, proto_version, ssl_params, timeout, user
+      ).connect(callback)
   end
+
+  return {
+    _start = function(...)
+      return connect()._start(...)
+    end,
+    connect = connect,
+    use = function(_db)
+      db = _db
+    end
+  }
+end
--- src/rethinkdb/convert_pseudotype.lua
+++ src/rethinkdb/convert_pseudotype.lua
@@ -2,68 +2,68 @@ local _r = require'rethinkdb.utilities'
 
 local errors = require'rethinkdb.errors'
 
-  local function convert_pseudotype(r, obj, opts)
-    if type(obj) == 'table' then
-      for key, value in pairs(obj) do
-        obj[key] = convert_pseudotype(r, value, opts)
-      end
-      -- An R_OBJECT may be a regular table or a 'pseudo-type' so we need a
-      -- second layer of type switching here on the obfuscated field '$reql_type$'
-      local reql_type = obj['$reql_type$']
-      if 'TIME' == reql_type then
-        local time_format = opts.time_format
-        if 'native' == time_format or not time_format then
-          if not (obj['epoch_time']) then
-            return _r.logger(r, errors.ReQLDriverError('pseudo-type TIME ' .. obj .. ' table missing expected field `epoch_time`.'))
-          end
-
-          -- We ignore the timezone field of the pseudo-type TIME table. JS dates do not support timezones.
-          -- By converting to a native date table we are intentionally throwing out timezone information.
-
-          -- field 'epoch_time' is in seconds but the Date constructor expects milliseconds
-          return obj['epoch_time']
-        elseif 'raw' == time_format then
-          return obj
-        else
-          return _r.logger(r, errors.ReQLDriverError('Unknown time_format run option ' .. opts.time_format .. '.'))
-        end
-      elseif 'GROUPED_DATA' == reql_type then
-        local group_format = opts.group_format
-        if 'native' == group_format or not group_format then
-          -- Don't convert the data into a map, because the keys could be tables which doesn't work in JS
-          -- Instead, we have the following format:
-          -- [ { 'group': <group>, 'reduction': <value(s)> } }, ... ]
-          local res = {}
-          for i, v in ipairs(obj['data']) do
-            res[i] = {
-              group = i,
-              reduction = v
-            }
-          end
-          obj = res
-        elseif 'raw' == group_format then
-          return obj
-        else
-          return _r.logger(r, errors.ReQLDriverError('Unknown group_format run option ' .. opts.group_format .. '.'))
-        end
-      elseif 'BINARY' == reql_type then
-        local binary_format = opts.binary_format
-        if 'native' == binary_format or not binary_format then
-          if not obj.data then
-            return _r.logger(r, errors.ReQLDriverError('pseudo-type BINARY table missing expected field `data`.'))
-          end
-          return _r.unb64(r, obj.data)
-        elseif 'raw' == binary_format then
-          return obj
-        else
-          return _r.logger(r, errors.ReQLDriverError('Unknown binary_format run option ' .. opts.binary_format .. '.'))
-        end
-      else
-        -- Regular table or unknown pseudo type
-        return obj
-      end
+local function convert_pseudotype(r, obj, opts)
+  if type(obj) == 'table' then
+    for key, value in pairs(obj) do
+      obj[key] = convert_pseudotype(r, value, opts)
     end
-    return obj
-  end
+    -- An R_OBJECT may be a regular table or a 'pseudo-type' so we need a
+    -- second layer of type switching here on the obfuscated field '$reql_type$'
+    local reql_type = obj['$reql_type$']
+    if 'TIME' == reql_type then
+      local time_format = opts.time_format
+      if 'native' == time_format or not time_format then
+        if not (obj['epoch_time']) then
+          return _r.logger(r, errors.ReQLDriverError('pseudo-type TIME ' .. obj .. ' table missing expected field `epoch_time`.'))
+        end
 
-  return convert_pseudotype
+        -- We ignore the timezone field of the pseudo-type TIME table. JS dates do not support timezones.
+        -- By converting to a native date table we are intentionally throwing out timezone information.
+
+        -- field 'epoch_time' is in seconds but the Date constructor expects milliseconds
+        return obj['epoch_time']
+      elseif 'raw' == time_format then
+        return obj
+      else
+        return _r.logger(r, errors.ReQLDriverError('Unknown time_format run option ' .. opts.time_format .. '.'))
+      end
+    elseif 'GROUPED_DATA' == reql_type then
+      local group_format = opts.group_format
+      if 'native' == group_format or not group_format then
+        -- Don't convert the data into a map, because the keys could be tables which doesn't work in JS
+        -- Instead, we have the following format:
+        -- [ { 'group': <group>, 'reduction': <value(s)> } }, ... ]
+        local res = {}
+        for i, v in ipairs(obj['data']) do
+          res[i] = {
+            group = i,
+            reduction = v
+          }
+        end
+        obj = res
+      elseif 'raw' == group_format then
+        return obj
+      else
+        return _r.logger(r, errors.ReQLDriverError('Unknown group_format run option ' .. opts.group_format .. '.'))
+      end
+    elseif 'BINARY' == reql_type then
+      local binary_format = opts.binary_format
+      if 'native' == binary_format or not binary_format then
+        if not obj.data then
+          return _r.logger(r, errors.ReQLDriverError('pseudo-type BINARY table missing expected field `data`.'))
+        end
+        return _r.unb64(r, obj.data)
+      elseif 'raw' == binary_format then
+        return obj
+      else
+        return _r.logger(r, errors.ReQLDriverError('Unknown binary_format run option ' .. opts.binary_format .. '.'))
+      end
+    else
+      -- Regular table or unknown pseudo type
+      return obj
+    end
+  end
+  return obj
+end
+
+return convert_pseudotype
