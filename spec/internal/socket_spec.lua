ae15e8bb70493d10ae180246d3efbaefb9e8ff38
Adam Grandquist <grandquista@gmail.com>
2016-05-14 19:20:20 -0700

    Run sanity test with socket wrapper.

--- spec/internal/socket_spec.lua
+++ spec/internal/socket_spec.lua
@@ -12,4 +12,58 @@ describe('socket', function()
   it('available functions', function()
     assert.are_same('function', type(socket))
   end)
+
+  it('closes repeatedly', function()
+    local client = assert(socket({}, nil, 28015, nil, 0))
+
+    finally(function() client.close() end)
+    
+    assert.is_false(client.isOpen())
+
+    client.close()
+
+    assert.is_false(client.isOpen())
+
+    client.open()
+
+    assert.is_true(client.isOpen())
+
+    client.close()
+
+    assert.is_false(client.isOpen())
+
+    client.close()
+
+    client.open()
+
+    assert.is_true(client.isOpen())
+
+    client.open()
+
+    assert.is_true(client.isOpen())
+  end)
+
+  it('connects', function()
+    local client = assert(socket({}, nil, 28015, nil, 0))
+
+    client.open()
+
+    assert.is_true(client.isOpen())
+
+    finally(function() client.close() end)
+
+    local idx = assert(client.send('\0\0', '\0\0\0\0\0\0\0\0\0\0'))
+
+    assert.are_equal(12, idx)
+
+    local message = assert(client.recv())
+
+    assert.are_equal(
+      'ERROR: Received an unsupported protocol version. This port is for ' ..
+      'RethinkDB queries. Does your client driver version not match the ' ..
+      'server?\n\0',
+      message)
+
+    client.close()
+  end)
 end)
