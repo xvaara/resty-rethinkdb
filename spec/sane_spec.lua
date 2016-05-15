08d4100edd3cf782b54db866af8064cb04722afb
Adam Grandquist <grandquista@gmail.com>
2016-05-14 17:43:12 -0700

    Check for a RethinkDB message in sanity test.

--- spec/sane_spec.lua
+++ spec/sane_spec.lua
@@ -4,7 +4,21 @@ insulate('safety net', function()
       local socket = require('socket')
       local client = assert(socket.tcp())
 
+      finally(function() client:close() end)
+
       assert(client:connect('localhost', 28015))
+
+      local idx = assert(client:send'\0\0\0\0\0\0\0\0\0\0\0\0')
+
+      assert.are_equal(idx, 12)
+
+      local message = assert(client:receive('*a'))
+
+      assert.are_equal(
+        message,
+        'ERROR: Received an unsupported protocol version. This port is for ' ..
+        'RethinkDB queries. Does your client driver version not match the ' ..
+        'server?\n\0')
 
       assert(require('rethinkdb'))
     end)
