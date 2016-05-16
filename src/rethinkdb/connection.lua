22d178b7564b35bab9e5ac8437aa4b5bfce2e997
Adam Grandquist <grandquista@gmail.com>
2016-05-16 05:51:56 -0700

    Handle possible errors in `builder:start` shortcut.

--- src/rethinkdb/connection.lua
+++ src/rethinkdb/connection.lua
@@ -7,16 +7,16 @@ local DEFAULT_USER = 'admin'
 local DEFAULT_AUTH_KEY = ''
 local DEFAULT_TIMEOUT = 20 -- In seconds
 
-return function(opts, _proto_version)
-  local port = opts.port or DEFAULT_PORT
-  local db = opts.db -- left nil if this is not set
-  local auth_key = opts.password or opts.auth_key or DEFAULT_AUTH_KEY
-  local user = opts.user or DEFAULT_USER
-  local timeout = opts.timeout or DEFAULT_TIMEOUT
-  local ssl_params = opts.ssl
+return function(connection_opts, _proto_version)
+  local port = connection_opts.port or DEFAULT_PORT
+  local db = connection_opts.db -- left nil if this is not set
+  local auth_key = connection_opts.password or connection_opts.auth_key or DEFAULT_AUTH_KEY
+  local user = connection_opts.user or DEFAULT_USER
+  local timeout = connection_opts.timeout or DEFAULT_TIMEOUT
+  local ssl_params = connection_opts.ssl
   local proto_version = _proto_version or current_protocol
-  local host = opts.host or DEFAULT_HOST
-  local r = opts.r or {}
+  local host = connection_opts.host or DEFAULT_HOST
+  local r = connection_opts.r or {}
 
   local function connect(callback)
     return connection_instance(
@@ -24,13 +24,26 @@ return function(opts, _proto_version)
       ).connect(callback)
   end
 
-  return {
-    _start = function(...)
-      return connect()._start(...)
-    end,
-    connect = connect,
-    use = function(_db)
-      db = _db
+  local function _start(term, callback, opts)
+    local function cb(err, conn)
+      if err then
+        if callback then
+          return callback(err)
+        end
+        error(err.message())
+      end
+      return conn._start(term, callback, opts)
     end
+    return connect(cb)
+  end
+
+  local function use(_db)
+    db = _db
+  end
+
+  return {
+    _start = _start,
+    connect = connect,
+    use = use
   }
 end
