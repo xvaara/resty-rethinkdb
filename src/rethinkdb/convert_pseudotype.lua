45fe105b6735134d66007852963f75f589296950
Adam Grandquist <grandquista@gmail.com>
2016-05-19 19:41:04 -0700

    Use table to replace if/else switch.

--- src/rethinkdb/convert_pseudotype.lua
+++ src/rethinkdb/convert_pseudotype.lua
@@ -3,70 +3,106 @@
 
 local _r = require'rethinkdb.utilities'
 
-local errors = require'rethinkdb.errors'
-
-local function convert_pseudotype(r, obj, opts)
-  if type(obj) == 'table' then
-    for key, value in pairs(obj) do
-      obj[key] = convert_pseudotype(r, value, opts)
-    end
-    -- An R_OBJECT may be a regular table or a 'pseudo-type' so we need a
-    -- second layer of type switching here on the obfuscated field '$reql_type$'
-    local reql_type = obj['$reql_type$']
-    if 'TIME' == reql_type then
-      local time_format = opts.time_format
-      if 'native' == time_format or not time_format then
-        if not (obj['epoch_time']) then
-          return _r.logger(r, errors.ReQLDriverError('pseudo-type TIME ' .. obj .. ' table missing expected field `epoch_time`.'))
-        end
-
-        -- We ignore the timezone field of the pseudo-type TIME table. JS dates do not support timezones.
-        -- By converting to a native date table we are intentionally throwing out timezone information.
-
-        -- field 'epoch_time' is in seconds but the Date constructor expects milliseconds
-        return obj['epoch_time']
-      elseif 'raw' == time_format then
-        return obj
-      else
-        return _r.logger(r, errors.ReQLDriverError('Unknown time_format run option ' .. opts.time_format .. '.'))
-      end
-    elseif 'GROUPED_DATA' == reql_type then
-      local group_format = opts.group_format
-      if 'native' == group_format or not group_format then
-        -- Don't convert the data into a map, because the keys could be tables which doesn't work in JS
-        -- Instead, we have the following format:
-        -- [ { 'group': <group>, 'reduction': <value(s)> } }, ... ]
-        local res = {}
-        for i, v in ipairs(obj['data']) do
-          res[i] = {
-            group = i,
-            reduction = v
-          }
-        end
-        obj = res
-      elseif 'raw' == group_format then
-        return obj
-      else
-        return _r.logger(r, errors.ReQLDriverError('Unknown group_format run option ' .. opts.group_format .. '.'))
-      end
-    elseif 'BINARY' == reql_type then
-      local binary_format = opts.binary_format
-      if 'native' == binary_format or not binary_format then
-        if not obj.data then
-          return _r.logger(r, errors.ReQLDriverError('pseudo-type BINARY table missing expected field `data`.'))
-        end
-        return _r.unb64(r, obj.data)
-      elseif 'raw' == binary_format then
-        return obj
-      else
-        return _r.logger(r, errors.ReQLDriverError('Unknown binary_format run option ' .. opts.binary_format .. '.'))
-      end
-    else
-      -- Regular table or unknown pseudo type
-      return obj
-    end
+-- Don't convert the data into a map, because the keys could be tables which doesn't work in JS
+-- Instead, we have the following format:
+-- [ { 'group': <group>, 'reduction': <value(s)> }, ... ]
+local function native_group(obj)
+  assert(obj.data, 'pseudo-type GROUPED_DATA table missing expected field `data`')
+  local res = {}
+  for group, reduction in pairs(obj.data) do
+    table.insert(res, {
+      group = group,
+      reduction = reduction
+    })
   end
+  return res
+end
+
+-- We ignore the timezone field of the pseudo-type TIME table. JS dates do not support timezones.
+-- By converting to a native date table we are intentionally throwing out timezone information.
+-- field 'epoch_time' is in seconds but the Date constructor expects milliseconds
+local function native_time(obj)
+  return assert(obj.epoch_time, 'pseudo-type TIME table missing expected field `epoch_time`')
+end
+
+local function raw(obj)
   return obj
+end
+
+local group_table = {
+  native = native_group,
+  raw = raw
+}
+
+local time_table = {
+  native = native_time,
+  raw = raw
+}
+
+local function convert_pseudotype(r, _obj, opts)
+  local function native_binary(obj)
+    assert(obj.data, 'pseudo-type BINARY table missing expected field `data`')
+    return _r.unb64(r, obj.data)
+  end
+
+  local binary_table = {
+    native = native_binary,
+    raw = raw
+  }
+
+  local fomat = opts.format or 'raw'
+  local binary_format, group_format, time_format =
+    opts.binary_format or fomat,
+    opts.group_format or fomat,
+    opts.time_format or fomat
+
+  local BINARY, GROUPED_DATA, TIME =
+    binary_table[binary_format],
+    group_table[group_format],
+    time_table[time_format]
+
+  if not BINARY then
+    return nil, 'Unknown binary_format run option ' .. binary_format
+  end
+
+  if not GROUPED_DATA then
+    return nil, 'Unknown group_format run option ' .. group_format
+  end
+
+  if not TIME then
+    return nil, 'Unknown time_format run option ' .. time_format
+  end
+
+  local conversion = {
+    BINARY = BINARY,
+    GROUPED_DATA = GROUPED_DATA,
+    TIME = TIME,
+  }
+
+  local function convert(obj)
+    if type(obj) == 'table' then
+      for key, value in pairs(obj) do
+        obj[key] = convert(value)
+      end
+
+      -- An R_OBJECT may be a regular table or a 'pseudo-type' so we need a
+      -- second layer of type switching here on the obfuscated field '$reql_type$'
+      local converter = conversion[obj['$reql_type$']]
+
+      if converter then
+        return converter(obj)
+      end
+    end
+    return obj
+  end
+
+  local status, res = pcall(convert, _obj)
+
+  if status then
+    return res
+  end
+
+  return nil, res
 end
 
 return convert_pseudotype
