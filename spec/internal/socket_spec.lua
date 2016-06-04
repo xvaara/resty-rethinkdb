09171cf1d292914b53957a5922642a30289af2fd
Adam Grandquist <grandquista@gmail.com>
2016-06-04 12:03:54 -0700

    Condense sanity specs.

--- spec/internal/socket_spec.lua
+++ spec/internal/socket_spec.lua
@@ -9,37 +9,24 @@ describe('socket', function()
     socket = nil
   end)
 
-  it('available functions', function()
-    assert.are_same('function', type(socket))
-  end)
-
   it('closes repeatedly', function()
     local client = assert.is_not_nil(socket({}, 'localhost', 28015, nil, 1))
-
-    finally(function() client.close() end)
-
     assert.is_false(client.is_open())
 
     client.close()
-
     assert.is_false(client.is_open())
 
     client.open()
-
     assert.is_true(client.is_open())
 
     client.close()
-
     assert.is_false(client.is_open())
 
     client.close()
-
     client.open()
-
     assert.is_true(client.is_open())
 
     client.open()
-
     assert.is_true(client.is_open())
   end)
 
@@ -52,18 +39,12 @@ describe('socket', function()
 
     finally(function() client.close() end)
 
-    local idx = assert.is_not_nil(client.send('\0\0', '\0\0\0\0\0\0\0\0\0\0'))
-
-    assert.are_equal(12, idx)
-
-    local message = assert.is_not_nil(client.recv())
+    assert.are_equal(12, client.send('\0\0', '\0\0\0\0\0\0\0\0\0\0'))
 
     assert.are_equal(
       'ERROR: Received an unsupported protocol version. This port is for ' ..
       'RethinkDB queries. Does your client driver version not match the ' ..
       'server?\n\0',
-      message)
-
-    client.close()
+      client.recv())
   end)
 end)
--- spec/sane_spec.lua
+++ spec/sane_spec.lua
@@ -8,17 +8,13 @@ insulate('safety net', function()
 
       assert.is_not_nil(client:connect('localhost', 28015))
 
-      local idx = assert.is_not_nil(client:send'\0\0\0\0\0\0\0\0\0\0\0\0')
-
-      assert.are_equal(idx, 12)
-
-      local message = assert.is_not_nil(client:receive('*a'))
+      assert.are_equal(12, client:send'\0\0\0\0\0\0\0\0\0\0\0\0')
 
       assert.are_equal(
-        message,
         'ERROR: Received an unsupported protocol version. This port is for ' ..
         'RethinkDB queries. Does your client driver version not match the ' ..
-        'server?\n\0')
+        'server?\n\0',
+        client:receive'*a')
 
       assert.is_not_nil(require('rethinkdb'))
     end)
