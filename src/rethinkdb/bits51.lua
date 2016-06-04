ff50bc81c50853b9aa23cc5eaa55cde6e8408d9b
Adam Grandquist <grandquista@gmail.com>
2016-06-04 10:12:45 -0700

    Make slow bits functions iterative.

--- src/rethinkdb/bits51.lua
+++ src/rethinkdb/bits51.lua
@@ -6,76 +6,49 @@
 -- @license Apache
 -- @copyright Adam Grandquist 2016
 
-local function pack_bits_impl(n, i, a, ...)
-  if a then
-    n = n + i
-  elseif a == nil then
-    return n
+local function pack_bits(a)
+  local n = 0
+  for i=1, #a do
+    if a[i] then
+      n = n + 2 ^ i
+    end
   end
-  return pack_bits_impl(n, i * 2, ...)
-end
-
-local function pack_bits(...)
-  return pack_bits_impl(0, 1, ...)
-end
-
-local function unpack_bits_impl(i, a, ...)
-  if a == 0 then
-    return ...
-  elseif a < i then
-    return unpack_bits_impl(i / 2, a - i, true, ...)
-  end
-  return unpack_bits_impl(i / 2, a, false, ...)
+  return n
 end
 
 local function unpack_bits(a)
-  local i = 1
-  while a < i do
-    i = i * 2
+  local n = {}
+  while a > 0 do
+    local i
+    a, i = math.modf(a / 2)
+    table.insert(n, i == 0.5)
   end
-  return unpack_bits_impl(i / 2, a)
+  local l = #n
+  for i=1, math.floor(l / 2) do
+    l = l - 1
+    n[i], n[l] = n[l], n[i]
+  end
+  return n
 end
 
-local function bor_impl__(a, b, i, ...)
-  local a_i = a[i]
-  local b_i = b[i]
-
-  if a_i == nil and b_i == nil then
-    return ...
+local function bor_impl(a, b)
+  local n = {}
+  for i=1, math.max(#a, #b) do
+    n[i] = a[i] or b[i]
   end
-
-  return bor_impl__(a, b, i + 1, ..., a_i or b_i)
+  return n
 end
 
-local function bor_impl(...)
-  local a = {...}
-
-  local function bor_impl_(...)
-    return bor_impl__(a, {...}, 1)
+local function bxor_impl(a, b)
+  local n = {}
+  for i=1, math.max(#a, #b) do
+    if a[i] then
+      n[i] = not b[i]
+    else
+      n[i] = b[i]
+    end
   end
-
-  return bor_impl_
-end
-
-local function bxor_impl__(a, b, i, ...)
-  local a_i = a[i]
-  local b_i = b[i]
-
-  if a_i == nil and b_i == nil then
-    return ...
-  end
-
-  return bxor_impl__(a, b, i + 1, ..., (a_i and not b_i) or (b_i and not a_i))
-end
-
-local function bxor_impl(...)
-  local a = {...}
-
-  local function bxor_impl_(...)
-    return bxor_impl__(a, {...}, 1)
-  end
-
-  return bxor_impl_
+  return n
 end
 
 local m = {}
@@ -85,7 +58,7 @@ local m = {}
 -- @int b integer bitfield 2
 -- @treturn int
 function m.bor(a, b)
-  return pack_bits(bor_impl(unpack_bits(a))(unpack_bits(b)))
+  return pack_bits(bor_impl(unpack_bits(a), unpack_bits(b)))
 end
 
 --- bitwise exclusive or of two values
@@ -93,7 +66,7 @@ end
 -- @int b integer bitfield 2
 -- @treturn int
 function m.bxor(a, b)
-  return pack_bits(bxor_impl(unpack_bits(a))(unpack_bits(b)))
+  return pack_bits(bxor_impl(unpack_bits(a), unpack_bits(b)))
 end
 
 --- normalize integer to bitfield
